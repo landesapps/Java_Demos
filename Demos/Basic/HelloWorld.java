54f91059af1048d5d2cdf2ac5307c57da6c5cc14
David Landes <wannaswordfight@gmail.com>
2012-09-06 22:26:41 -0600

    Added the Hello World demo

---
+++ Demos/Basic/HelloWorld.java
@@ -0,0 +1,12 @@
+//The location of the file
+package Demos.Basic;
+
+//Every java file must have a class with the same name as the file
+public class HelloWorld {
+
+    //The function that is called as soon as the file is executed
+    public static void main(String[] args) {
+        //Print "Hello World!" to the console
+        System.out.println("Hello World!");
+    }
+}
