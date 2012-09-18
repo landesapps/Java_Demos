e5d626930a111605a02b85492092ea7b762de76c
David Landes <david@davidlandes.net>
2012-09-18 15:45:20 -0600

    Added the comments to the Select demo

--- Demos/MySQL/Select.java
+++ Demos/MySQL/Select.java
@@ -1,3 +1,4 @@
+//Import the necessary SQL classes
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -7,17 +8,30 @@ import java.sql.SQLException;
 public class Select {
     public static void main(String[] args) {
         try {
+            //Create a connection to the mysql server
+            //First argument: jdbc:mysql://{host}/{database}
+            //Second argument: username
+            //Third argument: password
             Connection connect = DriverManager.getConnection("jdbc:mysql://localhost/java_demos", "username", "password");
+
+            //Create a prepared statement
+            //These allow you to send a query to a server and later define what the variables are supposed to be
             PreparedStatement stat = connect.prepareStatement("SELECT * FROM my_table WHERE id = ?");
 
+            //Assign the first ? in the query to be the string 1
             stat.setString(1, "1");
+            //Execute the query and store the results into a variable
             ResultSet results = stat.executeQuery();
 
+            //While there is another row, assign results to point to it
             while(results.next()) {
+                //Print out the results by getting the result with the column name
                 System.out.println("Column_1: " + results.getString("Column_1"));
             }
 
+        //If something goes wrong, please catch the exception
         } catch (SQLException exception) {
+            //Print the exception stack trace
             exception.printStackTrace();
         }
     }
