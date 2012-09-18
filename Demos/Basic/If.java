c12a101bdd8214a037dd602a5097aa13deebb943
David Landes <david@davidlandes.net>
2012-09-18 15:29:21 -0600

    Added the if statement demo

---
+++ Demos/Basic/If.java
@@ -0,0 +1,20 @@
+//Every java file must have a class with the same name as the file
+public class If {
+
+    //The function that is called as soon as the file is executed
+    public static void main(String[] args) {
+        //A boolean variable that is true
+        boolean comparison = true;
+
+        //Check to see if comparison is true
+        if(comparison) {
+            //Print out that comparison is true
+            System.out.println("comparison is true");
+
+        //Since comparison is false, then execute this code
+        } else {
+            //Print out that comparison is false
+            System.out.println("comparison is false");
+        }
+    }
+}
